adc8b4844c9d63595e4fe78d92b86df74562dc91
Mr. CHAN Sik Ham Timothy <sikhchan3@cityu.edu.hk>
2022-06-27 17:45:13 +0800

    patch

--- Main.java
+++ Main.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException
     {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
-        LocalDateTime systime=LocalDateTime.parse("2022-06-21 14:46:38.8", formatter);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SS");
+        LocalDateTime systime=LocalDateTime.parse("2022-06-21 14:46:38.80", formatter);
 
         System.out.println(systime.toString());
     }
